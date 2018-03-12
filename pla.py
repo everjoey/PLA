9644d65b1a73c4361a52f81ef3abca4c374baacf
Sun-Jung Lin <everjoey@gmail.com>
2018-03-12 02:41:56 +0000

    Update pla.py

--- pla.py
+++ pla.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 import numpy as np
-import randomimport cProfile
-from sklearn import linear_model
+import random
+import cProfile
 
 def perceptron(x_list, y_list):
-  w = np.zeros(4)
-  print(w)
-  while True:
-
-    
+	w = np.zeros(4)
+	print(w)
+	
+	
