d7a9b0cbf0482d26eb6f28088481bbe9f4519ad5
Sun-Jung Lin <everjoey@gmail.com>
2018-03-12 02:53:49 +0000

    Update pla.py

--- pla.py
+++ pla.py
@@ -6,3 +6,8 @@ import cProfile
 def perceptron(x_list, y_list):
 	w = np.zeros(4)
 	print(w)
+        while True:
+		for i in range(len(x_list)):
+			if np.sign(w.dot(x_list[i])) != y_list[i] and y_list[i] != 0:
+				
+			
