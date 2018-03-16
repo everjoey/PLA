f08d91d613583fca6ec5c169150e027c8ec65a0b
Sun-Jung Lin <everjoey@gmail.com>
2018-03-16 05:27:14 +0000

    Update pocket.py

--- pocket.py
+++ pocket.py
@@ -4,4 +4,11 @@ import random
 import cProfile
 
 def perceptron3(x_list, y_list, limit):
-	
+	wp = np.zeros(4)
+	print(wp)
+	wp_err_count = sum([np.sign(wp.dot(x_list[i])) != y_list[i] for i in range(len(x_list))])
+	print(wp_err_count)
+	for i in range(limit):
+		j = random.randrange(len(x_list))
+		x = x_list[j]
+		y = y_list[j]
